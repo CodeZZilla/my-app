14b30c5fed691681bf531f41306bf245f2f860d2
dimas <dimmaa9@gmail.com>
2022-04-18 18:23:30 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  // eslint-disable-line global-require
+  app.quit();
+}
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    frame: 0,
+    autoHideMenuBar: true,
+    resizable: false,
+    width: 800,
+    height: 600,
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  // mainWindow.webContents.openDevTools();
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
