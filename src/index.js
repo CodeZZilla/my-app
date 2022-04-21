067b8589fd06c75931699763d2142cd313dd4576
dimas <dimmaa9@gmail.com>
2022-04-21 11:56:58 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,39 +1,61 @@
-const { app, BrowserWindow } = require('electron');
+const {app, BrowserWindow} = require('electron');
 const path = require('path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
-  // eslint-disable-line global-require
-  app.quit();
+    // eslint-disable-line global-require
+    app.quit();
 }
 
 const createWindow = () => {
-  const mainWindow = new BrowserWindow({
-    frame: 0,
-    transparent:true,
-    backgroundColor: "rgba(255,0,0,0)",
-    autoHideMenuBar: true,
-    resizable: false,
-    width: 800,
-    height: 600,
-  });
+    const mainWindow = new BrowserWindow({
+        frame: 0,
+        transparent: true,
+        backgroundColor: "rgba(255,0,0,0)",
+        autoHideMenuBar: true,
+        resizable: false,
+        width: 300,
+        height: 350,
+        x: 0,
+        y: 0
+    });
+    mainWindow.setAlwaysOnTop(true)
+    mainWindow.setVisibleOnAllWorkspaces(true)
+    mainWindow.setMinimizable(false)
 
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+    mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
-  // mainWindow.webContents.openDevTools();
+    // let start = 1
+    // let flag = true
+    // setInterval(() => {
+    //     if (flag) {
+    //         mainWindow.setPosition(start++, 0)
+    //         if (start === 200) {
+    //             flag = false
+    //         }
+    //     } else {
+    //         mainWindow.setPosition(start--, 0)
+    //         if (start === 1) {
+    //             flag = true
+    //         }
+    //     }
+    // }, 2)
+    // mainWindow.webContents.openDevTools();
 };
+
+app.dock.hide()
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 });
 
